6f1916d12a96ed5555202ccd6107ea4ec76748ad
Rico Mitchell <ricomitch@icloud.com>
2019-09-28 05:33:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,9 @@ end
 end
 
 def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
+  updated_hash = hash
+  if updated_hash[key]
+    updated_hash[key] += 1
+  else
+    updated_hash[key] = 1
 end
